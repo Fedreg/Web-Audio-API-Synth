23b82cb7c51916cd21357e2f03919433cde7397c
Fed Reggiardo <Fedreg@users.noreply.github.com>
2016-08-19 02:13:53 -0500

    Working on connecting entered text message to Vue component.

--- scripts.js
+++ scripts.js
@@ -22,44 +22,58 @@ Vue.component('keyboard', {
       		frequencies: []
     	};
   	},
+  	events: { 'playNote': function(note) {
+  		play;
+  		}
+  	},
 	methods: {
-	
-		play(note){
-		//adjusts frequency played by 50%, 100% or 200% 
-		var octave = this.octave;
- 		//determines note duration
- 		var sustain = parseInt(this.sustain);
- 		//determines color to change to 
- 		var colorChanger = '#'+Math.floor(Math.random()*16777215).toString(16);//this.color;		//produces sound
-		var oscillator = context.createOscillator();
-		//oscillator wave type (sine, triangle, square, or sawtooth)
-	 	oscillator.type = 'square';
-		//create volume controller
-		var gainNode = context.createGain();
-		//connect signal to audio to gain; gain to compressor (compressor to output)
-	 	oscillator.connect(gainNode);
-	 	gainNode.connect(compressor); 	
-		//sets oscillator frequency
-	 	oscillator.frequency.value = note * octave;
-	 	console.log(oscillator.frequency.value);
-	 	//initialize gain at 0 and ramp up to full volume very quikcly (prevents audible 'pop')
-	 	gainNode.gain.value = 0
-	 	var quickFadeIn = gainNode.gain.setTargetAtTime(.15, context.currentTime, 0.1);
-	 	oscillator.start(context.currentTime + .05);
- 		var quickFadeOut = gainNode.gain.setTargetAtTime(0, context.currentTime + sustain, 0.1);
- 		//references current element to change its color
- 		var currentDiv = event.currentTarget;
- 		//change background color for durarion of note length
-		currentDiv.style.backgroundColor = colorChanger;
-		setTimeout(function () {
-    			currentDiv.style.backgroundColor = 'rgba(0,0,0,0)';
- 		}, 1000 * sustain);  
-	}}
+		handle: function (msg) {
+			alert(msg);
+			},
+		
+		play: function (note){
+			//adjusts frequency played by 50%, 100% or 200% 
+			var octave = this.octave;
+	 		//determines note duration
+	 		var sustain = parseInt(this.sustain);
+	 		//determines color to change to 
+	 		var colorChanger = '#'+Math.floor(Math.random()*16777215).toString(16);//this.color;		//produces sound
+			var oscillator = context.createOscillator();
+			//oscillator wave type (sine, triangle, square, or sawtooth)
+		 	oscillator.type = 'square';
+			//create volume controller
+			var gainNode = context.createGain();
+			//connect signal to audio to gain; gain to compressor (compressor to output)
+		 	oscillator.connect(gainNode);
+		 	gainNode.connect(compressor); 	
+			//sets oscillator frequency
+		 	oscillator.frequency.value = note * octave;
+		 	console.log(oscillator.frequency.value);
+		 	//initialize gain at 0 and ramp up to full volume very quikcly (prevents audible 'pop')
+		 	gainNode.gain.value = 0
+		 	var quickFadeIn = gainNode.gain.setTargetAtTime(.15, context.currentTime, 0.1);
+		 	oscillator.start(context.currentTime + .05);
+	 		var quickFadeOut = gainNode.gain.setTargetAtTime(0, context.currentTime + sustain, 0.1);
+	 		//references current element to change its color
+	 		var currentDiv = event.currentTarget;
+	 		//change background color for durarion of note length
+			currentDiv.style.backgroundColor = colorChanger;
+			setTimeout(function () {
+	    		currentDiv.style.backgroundColor = 'rgba(0,0,0,0)';
+	 		}, 1000 * sustain);  
+		}
+	}
 });
 	
 var vm = new Vue({
 	el: '#app',
+	methods: {
+		playNote: function (message) {
+			this.$broadcast('play(note)');
+			console.log(message)
+			}},
 	data: {
+		message: '',
 		frequencies: [
 		 	{note: 130.81}, //c
 		 	{note: 146.83}, //d
